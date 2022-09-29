f741d6012805fbb05129ceac5826797303c33854
gcoadour <47463874+gcoadour@users.noreply.github.com>
2022-09-29 16:37:41 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular';
-  hourscolor =["red","green","orange","green","green","orange","green","green","orange","green","green","orange","green"]
+  hoursColor = [
+    1, 2, 3, 1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 2, 1, 3, 2, 1, 2, 1, 1, 1, 1, 1, 1,
+  ];
 }
